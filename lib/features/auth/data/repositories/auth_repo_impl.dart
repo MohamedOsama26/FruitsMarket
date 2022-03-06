bbab298d9f220c20cf5cc78ae3c1738adb023075
MohamedOsama26 <mohamed.99.kandel@gmail.com>
2022-03-06 14:50:31 +0200

    google fb auth

--- lib/features/auth/data/repositories/auth_repo_impl.dart
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -1,38 +1,42 @@
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruit_markit/features/auth/domain/repositories/auth_repo.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:dartz/dartz.dart';
+
 //
 //
 class AuthRepoImpl extends AuthRepo {
 //
   @override
-  Future completeInformation({String? name, String? phoneNumber, String? address}) {
-
+  Future completeInformation(
+      {String? name, String? phoneNumber, String? address}) {
     throw UnimplementedError();
   }
+
 //
   @override
   Future loginWithFacebook() async {
-//   //
-//   //   // Trigger the sign-in flow
-//   //   final LoginResult loginResult = await FacebookAuth.instance.login();
-//   //
-//   //   // Create a credential from the access token
-//   //   final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(loginResult.accessToken!.token);
-//   //
-//   //   // Once signed in, return the UserCredential
-//   //   return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
   }
 
   @override
-  Future loginWithGoogle()  async {
-
+  Future<Either<Exception, UserCredential>> loginWithGoogle() async {
+    try {
       // Trigger the authentication flow
       final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
       // Obtain the auth details from the request
-      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
 
       // Create a new credential
       final credential = GoogleAuthProvider.credential(
@@ -41,9 +45,10 @@ class AuthRepoImpl extends AuthRepo {
       );
 
       // Once signed in, return the UserCredential
-      return await FirebaseAuth.instance.signInWithCredential(credential);
-
-
+      return Right(
+          await FirebaseAuth.instance.signInWithCredential(credential));
+    } catch (e) {
+      return Left(Exception('Something went wrong : $e'));
+    }
   }
-
 }
